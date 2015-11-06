c3846fbd0865d4d95eafd757faeb8d0a40926504
Martin Lehmann <martin.lehmann@finn.no>
2015-11-06 09:50:27 +0100

    Support formatting with time and month as number.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,79 @@
 import IllegalArgumentException from './IllegalArgumentException';
 
-export default function format(date) {
+const abbreviatedMonthNames = [
+  'jan',
+  'feb',
+  'mar',
+  'apr',
+  'mai',
+  'jun',
+  'jul',
+  'aug',
+  'sep',
+  'okt',
+  'nov',
+  'des',
+];
+
+const padWithZero = number => {
+  if (number < 10) {
+    return `0${number}`;
+  }
+
+  return `${number}`;
+};
+
+const addTime = (dateWithoutTime, hours, minutes) => {
+  return `${dateWithoutTime} ${hours}:${minutes}`;
+};
+
+const formatDefault = (fullYearNumber, monthNumber, dateInMonthNumber) => {
+  const dateInMonth = padWithZero(dateInMonthNumber);
+  const month = abbreviatedMonthNames[monthNumber];
+  const year = `${fullYearNumber}`;
+
+  return `${dateInMonth}. ${month} ${year}`;
+};
+
+export default function format(date, options) {
   if (!date || !(date instanceof Date)) {
     throw new IllegalArgumentException(`Parameter ${date} is not an instance of Date`);
   }
 
-  return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+  const dateInMonthNumber = date.getDate();
+  const monthNumber = date.getMonth();
+  const fullYearNumber = date.getFullYear();
+
+  if (options) {
+    if (options.monthAsNumber) {
+      const dateInMonth = padWithZero(dateInMonthNumber);
+      const month = padWithZero(monthNumber + 1);
+      const year = `${fullYearNumber}`;
+
+      const stringWithoutTime = `${dateInMonth}.${month}.${year}`;
+
+      if (options.showTime) {
+        const hours = padWithZero(date.getHours());
+        const minutes = padWithZero(date.getMinutes());
+
+        return addTime(stringWithoutTime, hours, minutes);
+      }
+
+      return stringWithoutTime;
+    }
+
+    const defaultDateString = formatDefault(fullYearNumber, monthNumber, dateInMonthNumber);
+
+    if (options.showTime) {
+      const hours = padWithZero(date.getHours());
+      const minutes = padWithZero(date.getMinutes());
+
+      return addTime(defaultDateString, hours, minutes);
+    }
+
+    return defaultDateString;
+  }
+
+  return formatDefault(fullYearNumber, monthNumber, dateInMonthNumber);
 }
 
